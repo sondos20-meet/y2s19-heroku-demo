0e890b8d073a8b513b24db3c2672f0cbcf6760e8
myname21-meet <myname21@meet.mit.edu>
2019-07-31 06:01:18 -0400

    another thing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	favoritefood = ['everythig','everysing','everydrink']
+	favoritefood = ['everythig','everysing','everydrink','Rani melzer']
 	return render_template(
 		"index.html",
 		favoritefood=favoritefood,

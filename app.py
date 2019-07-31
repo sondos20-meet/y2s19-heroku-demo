7608349f7fb6ac0a51dfd233de7c452b74ca7cf9
myname21-meet <myname21@meet.mit.edu>
2019-07-31 06:07:59 -0400

    workkkgit add .

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, render_template
+from flask import session as login_session
+
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route('/')
-def home_page():
-	favoritefood = ['everythig','everysing','everydrink','Rani melzer']
-	return render_template(
-		"index.html",
-		favoritefood=favoritefood,
-		opposite_day=True)
+def home():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return render_template(
+        'hello.html', n = ‘something’)
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
+    app.run(debug=True)

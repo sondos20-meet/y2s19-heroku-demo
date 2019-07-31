757c7865a224b5946ee0d16c9fb3e273b93f0955
myname21-meet <myname21@meet.mit.edu>
2019-07-31 06:12:03 -0400

    plz

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
-from flask import Flask, request, redirect, render_template
-from flask import session as login_session
-
+from flask import Flask, render_template
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/hello')
-def hello():
-    return render_template(
-        'hello.html', n = ‘something’)
-
+def home_page():
+	favoritefood = ['everythig','everysing','everydrink','Rani melzer']
+	return render_template(
+		"index.html",
+		favoritefood=favoritefood,
+		opposite_day=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug = True)
+
